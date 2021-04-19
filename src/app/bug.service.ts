e9aebc380ad4d42af062c95651a77801075eae35
Aishwarya Srivastava <aishwaryaaaaaaaaaaa@gmail.com>
2021-04-19 17:45:27 +0530

    added search by name and status

---
+++ src/app/bug.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Bug } from './Bug';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BugService {
+
+  constructor(private http: HttpClient) { }
+  save(bug: Bug) {
+    return this.http.post('http://localhost:8085/bug', bug, {
+      headers: { "content-type": 'application/json' },
+      responseType:"text"
+    });
+  }
+  getAllBugs(){
+    return this.http.get('http://localhost:8085/bug')
+  }
+
+  getBugName(bugname:string  ){
+    return this.http.get('http://localhost:8085/bug/name'+'/'+bugname);
+  }
+}
